af21172c1b02eaae4a278d62a1a96e30669b2504
Vinicius Maciel <vinicius.nmaciel@gmail.com>
2022-03-31 00:11:14 -0300

    arquivos para trablhar com o Docker

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -25,3 +26,7 @@ def metodo_post_params():
     meuparametro = parametros["meuparametro"]
 
     return jsonify(parametroenviado = meuparametro)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
